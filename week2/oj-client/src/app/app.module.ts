7a3abcfd6b8432098840c94d4e5c17fe7fcc1e1a
Tony <jliang1120tony@gmail.com>
2018-04-28 18:16:18 -0700

    imported ReactiveFormsModule to app.module.ts, but the [fromControl] data binding problem still exits

--- week2/oj-client/src/app/app.module.ts
+++ week2/oj-client/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
